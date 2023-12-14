bd620232baee12b4926ff4df61354c804a13ee9a
Parth Thakral <parthth@amdocs.com>
2023-12-14 10:30:05 +0530

    Adding DB Connectivity

--- src/main/java/com/productservice/productservice/models/BaseModel.java
+++ src/main/java/com/productservice/productservice/models/BaseModel.java
@@ -1,10 +1,14 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass  //to not make object of this class and pass its attributes in the child classes
 public class BaseModel {
+    @Id
     private Long id;
 }
--- src/main/java/com/productservice/productservice/models/Product.java
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -1,16 +1,28 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
 //    private Long id;
     private String title;
-    private String desc;
+    private String description;
     private int price;
     private String image;
+
+    //Category isn't a primitive attribute, its a relation
+    @ManyToOne
     private Category category;
 
 }
+
+/*
+Cardinality b/w in product ---- category
+
+
+ */
