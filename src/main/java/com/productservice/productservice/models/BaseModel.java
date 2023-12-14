b988fbf399607a98e5bcea2358982093cd4c8d97
Parth Thakral <parthth@amdocs.com>
2023-12-14 12:31:20 +0530

    Setting UUID

--- src/main/java/com/productservice/productservice/models/BaseModel.java
+++ src/main/java/com/productservice/productservice/models/BaseModel.java
@@ -1,14 +1,22 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass  //to not make object of this class and pass its attributes in the child classes
 public class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue(generator = "generator_name")
+    @GenericGenerator(name = "geneartor_name" )
+    @Column(name = "id", columnDefinition = "binary(16)" , nullable = false, updatable = false)
+    private UUID id;
 }
